f2a13385bb9f8f483adc2784db652d3788810678
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-09-15 22:02:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const line = document.getElementById("line");
+let count = 0;
+setInterval(()=> {
+	count += 20;
+	line.style.transform = `rotate(${count}deg)`;
+}, 20)
